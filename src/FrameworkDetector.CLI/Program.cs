3878f82cebf6405ac8e189d9f52cd9bd8831ff2b
Michael Hawker <24302614+michael-hawker@users.noreply.github.com>
2025-08-11 11:54:18 -0700

    Add initial command line processing via System.CommandLine
    TODO: Need to do next step of quickstart to enable help

--- src/FrameworkDetector.CLI/Program.cs
+++ src/FrameworkDetector.CLI/Program.cs
@@ -2,6 +2,8 @@
 // Licensed under the MIT License.
 
 using System;
+using System.CommandLine;
+using System.CommandLine.Parsing;
 
 namespace FrameworkDetector.CLI;
 
@@ -12,7 +14,42 @@ internal static class Program
     {
         AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
 
+        Option<int> pidOption = new("--processId", "--pid")
+        {
+            Description = "The process ID to inspect.",
+            Required = true,
+        };
+
+        RootCommand rootCommand = new("Framework Detector");
+        rootCommand.Options.Add(pidOption);
+
+        ParseResult? parseResult = null;
+        try
+        {
+            parseResult = rootCommand.Parse(args);
+            if (parseResult.GetValue(pidOption) is int processId)
+            {
+                InspectProcess(processId);
+            }
+        } 
+        catch (InvalidOperationException)
+        {
+            // Display any command argument errors
+            foreach (ParseError parseError in parseResult?.Errors ?? Array.Empty<ParseError>())
+            {
+                Console.Error.WriteLine(parseError.Message);
+            }
+
+            return 1;
+        }
+
         return 0;
+    }
+
+    private static void InspectProcess(int processId)
+    {
+        // TODO: Probably have this elsewhere to be called
+        Console.WriteLine($"Inspecting process with ID: {processId}");
     }
 
     private static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
