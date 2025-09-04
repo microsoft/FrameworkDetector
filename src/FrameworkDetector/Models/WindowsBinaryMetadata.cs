1e6515be011a77b5f50e7f8c6b2b636f94006680
Jon Thysell <jthysell@microsoft.com>
2025-09-04 14:57:46 -0700

    Fix redirect of 32bit dlls from System32 to SysWOW64

--- src/FrameworkDetector/Models/WindowsBinaryMetadata.cs
+++ src/FrameworkDetector/Models/WindowsBinaryMetadata.cs
@@ -29,6 +29,23 @@ public record WindowsBinaryMetadata(string Filename,
             return await Task.FromCanceled<WindowsBinaryMetadata?>(cancellationToken);
         }
 
+        if (!Path.Exists(filename))
+        {
+            // Try to see if we're looking for a binary under C:\Windows\System32 but really it's under C:\Windows\SysWOW64
+            var system32 = Environment.GetFolderPath(Environment.SpecialFolder.System);
+            var syswow64 = Environment.GetFolderPath(Environment.SpecialFolder.SystemX86);
+
+            var oldPath = filename;
+            if (oldPath.StartsWith(system32, StringComparison.InvariantCultureIgnoreCase))
+            {
+                var newPath = Path.Join(syswow64, Path.GetRelativePath(system32, oldPath));
+                if (File.Exists(newPath))
+                {
+                    filename = newPath;
+                }
+            }
+        }
+
         var fileVersionInfo = FileVersionInfo.GetVersionInfo(filename);
 
         return new WindowsBinaryMetadata(Path.GetFileName(fileVersionInfo.FileName), 
