2d5b0f63635776ba804008262c66a1a5f0885a4c
Jon Thysell <jthysell@microsoft.com>
2025-10-01 14:11:36 -0700

    Add ContainsImportedFunctionCheck with tests

---
+++ src/FrameworkDetector.Test/Checks/ContainsImportedFunctionCheckTest.cs
@@ -0,0 +1,78 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using static FrameworkDetector.Checks.ContainsImportedFunctionCheck;
+using FrameworkDetector.Models;
+
+namespace FrameworkDetector.Test.Checks;
+
+[TestClass]
+public class ContainsImportedFunctionCheckTest() : CheckTestBase<ContainsImportedFunctionArgs, ContainsImportedFunctionData>(GetCheckRegistrationInfo)
+{
+    [TestMethod]
+    [DataRow("TestModule.dll", "")]
+    [DataRow("TestModule.dll", "TestFunctionName")]
+    [DataRow("TestModule.dll", "TestFunctionName", "TestModule.dll", "")]
+    [DataRow("TestModule.dll", "TestFunctionName", "TestModule.dll", "TestFunction")]
+    [DataRow("TestModule.dll", "TestFunctionName", "TestModule.dll", "Function")]
+    [DataRow("TestModule.dll", "TestFunctionName", "TestModule.dll", "FunctionName")]
+    public async Task ContainsImportedFunctionCheck_FunctionNameFoundTest(string moduleName, string functionName, string? moduleNameToCheck = null, string? functionNameToCheck = null)
+    {
+        await RunFunctionNameCheck(moduleName, [functionName], moduleNameToCheck ?? moduleName, functionNameToCheck ?? functionName, DetectorCheckStatus.CompletedPassed, moduleName, functionName);
+    }
+
+    [TestMethod]
+    [DataRow("TestModule.dll", "", "TestModule.dll", "TestFunctionName")]
+    [DataRow("TestModule.dll", "TestFunctionName", "TestModule.dll", "WrongFunctionName")]
+    public async Task ContainsImportedFunctionCheck_FunctionNameNotFoundTest(string actualModuleName, string actualFunctionName, string moduleNameToCheck, string functionNameToCheck)
+    {
+        await RunFunctionNameCheck(actualModuleName, [actualFunctionName], moduleNameToCheck, functionNameToCheck, DetectorCheckStatus.CompletedFailed, null, null);
+    }
+
+    private async Task RunFunctionNameCheck(string actualModuleName, string[] actualFunctionNames, string moduleNameToCheck, string functionNameToCheck, DetectorCheckStatus expectedCheckStatus, string? expectedModuleName, string? expectedFunctionName)
+    {
+        var actualImportedFunctions = new ProcessImportedFunctionsMetadata(actualModuleName, actualFunctionNames.Select(afn => new ProcessFunctionMetadata(afn)).ToArray());
+        var args = new ContainsImportedFunctionArgs(moduleNameToCheck, functionNameToCheck);
+
+        ContainsImportedFunctionData? expectedOutput = expectedModuleName is not null && expectedFunctionName is not null ? new ContainsImportedFunctionData(new ProcessImportedFunctionsMetadata(expectedModuleName, [new ProcessFunctionMetadata(expectedFunctionName)])) : null;
+
+        var cts = new CancellationTokenSource();
+
+        await RunTest([actualImportedFunctions], args, expectedCheckStatus, expectedOutput, cts.Token);
+    }
+
+    private async Task RunTest(ProcessImportedFunctionsMetadata[]? actualImportedFunctions, ContainsImportedFunctionArgs args, DetectorCheckStatus expectedCheckStatus, ContainsImportedFunctionData? expectedOutput, CancellationToken cancellationToken)
+    {
+        var dataSources = GetTestProcessDataSource(new ProcessMetadata(nameof(ContainsImportedFunctionCheckTest), ImportedFunctions: actualImportedFunctions));
+        await RunCheck_ValidArgsAsync(dataSources, args, expectedCheckStatus, expectedOutput, cancellationToken);
+    }
+
+    protected override void ValidateOutputData(ContainsImportedFunctionData? expected, ContainsImportedFunctionData? actual)
+    {
+        if (expected is not null && actual is not null)
+        {
+            var expectedFunction = expected.Value.ImportedFunctionFound;
+            var actualFunction = actual.Value.ImportedFunctionFound;
+
+            Assert.AreEqual(expectedFunction.ModuleName, actualFunction.ModuleName);
+            Assert.AreEqual(expectedFunction.Functions is null, actualFunction.Functions is null);
+
+            if (expectedFunction.Functions is not null)
+            {
+                Assert.AreEqual(expectedFunction.Functions.Length, actualFunction.Functions?.Length ?? 0);
+
+                for (int i = 0; i < expectedFunction.Functions.Length; i++)
+                {
+                    Assert.AreEqual(expectedFunction.Functions[i], actualFunction.Functions?[i]);
+                }
+            }
+        }
+    }
+}
---
+++ src/FrameworkDetector/Checks/ContainsImportedFunctionCheck.cs
@@ -0,0 +1,175 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+using FrameworkDetector.DataSources;
+using FrameworkDetector.Engine;
+using FrameworkDetector.Models;
+
+namespace FrameworkDetector.Checks;
+
+/// <summary>
+/// CheckDefinition extension for looking for a specific imported function, present within the PE headers of a process binary.
+/// </summary>
+public static class ContainsImportedFunctionCheck
+{
+    /// <summary>
+    /// Get registration information defining <see cref="ContainsImportedFunctionCheck"/>.
+    /// </summary>
+    internal static CheckRegistrationInfo<ContainsImportedFunctionArgs, ContainsImportedFunctionData> GetCheckRegistrationInfo(ContainsImportedFunctionArgs args)
+    {
+        return new(
+            Name: nameof(ContainsImportedFunctionCheck),
+            Description: args.GetDescription(),
+            DataSourceIds: [ProcessDataSource.Id],
+            PerformCheckAsync);
+    }
+
+    /// <summary>
+    /// Input arguments for <see cref="ContainsImportedFunctionCheck"/>.
+    /// </summary>
+    /// <param name="moduleName">An imported function's module name must match this text, if specified.</param>
+    /// <param name="functionName">An imported function's function name must contain this text, if specified.</param>
+    /// <param name="delayLoaded">An imported function must be delay loaded (or not), if specified.</param>
+    public readonly struct ContainsImportedFunctionArgs(string? moduleName = null, string? functionName = null, bool? delayLoaded = null) : ICheckArgs
+    {
+        public string? ModuleName { get; } = moduleName;
+
+        public string? FunctionName { get; } = functionName;
+
+        public bool? DelayLoaded { get; } = delayLoaded;
+
+        public string GetDescription()
+        {
+            var descriptionSB = new StringBuilder("Find ");
+
+            if (DelayLoaded is not null && DelayLoaded.Value)
+            {
+                descriptionSB.Append("delay-loaded ");
+            }
+
+            descriptionSB.Append("imported function ");
+
+            bool nameAdded = false;
+            if (FunctionName is not null)
+            {
+                descriptionSB.AppendFormat("{0}", FunctionName);
+                nameAdded = true;
+            }
+
+            if (ModuleName is not null)
+            {
+                if (nameAdded)
+                {
+                    descriptionSB.Append(' ');
+                }
+                descriptionSB.AppendFormat("in {0}", ModuleName);
+            }
+
+            return descriptionSB.ToString();
+        }
+
+        public void Validate() => ArgumentNullException.ThrowIfNull(ModuleName ?? FunctionName, nameof(ContainsImportedFunctionArgs));
+    }
+
+    /// <summary>
+    /// Output data for <see cref="ContainsImportedFunctionCheck"/>.
+    /// </summary>
+    /// <param name="importedFunctionFound">The imported function found.</param>
+    public readonly struct ContainsImportedFunctionData(ProcessImportedFunctionsMetadata importedFunctionFound)
+    {
+        public ProcessImportedFunctionsMetadata ImportedFunctionFound { get; } = importedFunctionFound;
+    }
+
+    extension(DetectorCheckGroup @this)
+    {
+        /// <summary>
+        /// Checks for an imported function in the PE headers of the process.
+        /// </summary>
+        /// <param name="moduleName">An imported function's module name must match this text, if specified.</param>
+        /// <param name="functionName">An imported function's function name must contain this text, if specified.</param>
+        /// <param name="delayLoaded">An imported function must be delay loaded (or not), if specified.</param>
+        /// <returns></returns>
+        public DetectorCheckGroup ContainsImportedFunction(string? moduleName = null, string? functionName = null, bool? delayLoaded = null)
+        {
+            // This copies over an entry pointing to this specific check's registration with the metadata requested by the detector.
+            // The metadata along with the live data sources (as indicated by the registration)
+            // will be passed into the PerformCheckAsync method below to do the actual check.
+
+            var args = new ContainsImportedFunctionArgs(moduleName, functionName, delayLoaded);
+            args.Validate();
+
+            @this.AddCheck(new CheckDefinition<ContainsImportedFunctionArgs, ContainsImportedFunctionData>(GetCheckRegistrationInfo(args), args));
+
+            return @this;
+        }
+    }
+
+    //// Actual check code run by engine
+
+    public static async Task PerformCheckAsync(CheckDefinition<ContainsImportedFunctionArgs, ContainsImportedFunctionData> definition, DataSourceCollection dataSources, DetectorCheckResult<ContainsImportedFunctionArgs, ContainsImportedFunctionData> result, CancellationToken cancellationToken)
+    {
+        if (dataSources.TryGetSources(ProcessDataSource.Id, out IProcessDataSource[] processes))
+        {
+            result.CheckStatus = DetectorCheckStatus.InProgress;
+
+            foreach (var process in processes)
+            {
+                var importedFunctions = process.ProcessMetadata?.ImportedFunctions;
+                if (importedFunctions is not null)
+                {
+                    foreach (var importedFunction in importedFunctions)
+                    {
+                        await Task.Yield();
+
+                        if (cancellationToken.IsCancellationRequested)
+                        {
+                            result.CheckStatus = DetectorCheckStatus.Canceled;
+                            break;
+                        }
+
+                        var moduleNameMatch = definition.CheckArguments.ModuleName is null || string.Equals(definition.CheckArguments.ModuleName, importedFunction.ModuleName, StringComparison.InvariantCultureIgnoreCase);
+
+                        if (moduleNameMatch)
+                        {
+                            var functionMatch = (definition.CheckArguments.FunctionName is null && definition.CheckArguments.DelayLoaded is null)
+                                             || (importedFunction.Functions is not null && importedFunction.Functions.Any(f =>
+                            {
+                                return (definition.CheckArguments.FunctionName is null || f.Name.Contains(definition.CheckArguments.FunctionName, StringComparison.InvariantCultureIgnoreCase))
+                                    && (definition.CheckArguments.DelayLoaded is null || definition.CheckArguments.DelayLoaded == f.DelayLoaded);
+                            }));
+
+                            if (functionMatch)
+                            {
+                                result.OutputData = new ContainsImportedFunctionData(importedFunction);
+                                result.CheckStatus = DetectorCheckStatus.CompletedPassed;
+                                break;
+                            }
+                        }
+                    }
+                }
+
+                // Stop evaluating other process data sources if we've gotten a pass or cancel
+                if (result.CheckStatus != DetectorCheckStatus.InProgress)
+                {
+                    break;
+                }
+            }
+
+            if (result.CheckStatus == DetectorCheckStatus.InProgress)
+            {
+                result.CheckStatus = DetectorCheckStatus.CompletedFailed;
+            }
+        }
+        else
+        {
+            // No CheckInput = Error
+            result.CheckStatus = DetectorCheckStatus.Error;
+        }
+    }
+}
